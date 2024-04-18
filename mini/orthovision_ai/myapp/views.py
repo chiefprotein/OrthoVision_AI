ed0d983386857a61c118bf7db0d891de2ca46b4d
chiefprotein <jenilbiju@gmail.com>
2024-04-19 00:52:21 +0530

    50%

--- mini/orthovision_ai/myapp/views.py
+++ mini/orthovision_ai/myapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.http import HttpResponse
 from .forms import XrayImgForm
 from .models import XrayImages
+from django.db import IntegrityError
 def Login(request):
     if request.method == "POST":
         username = request.POST.get("username")
@@ -18,8 +20,27 @@ def Login(request):
             storage.used = True
     return render(request, 'myapp/login.html')
 def signup(request):
-    context={}
-    return render(request,'myapp/Signup.html',context)
+    if request.method=="POST":
+        email=request.POST.get("email")
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+        confirm_password=request.POST.get("confirm_password")
+        if password!=confirm_password:
+            messages.error(request, ("Confirmed password was incorrect"))
+            storage = messages.get_messages(request)
+            storage.used = True
+        else:
+            try:
+                user=User.objects.create_user(username,email,password)
+                user.save()
+                messages.success(request, ("New User successfully created."))
+                storage = messages.get_messages(request)
+                storage.used = True
+            except IntegrityError:
+                messages.error(request, ("Username already exists"))
+                storage = messages.get_messages(request)
+                storage.used = True
+    return render(request,'myapp/Signup.html')
 def upload(request):
  
     if request.method == 'POST':
